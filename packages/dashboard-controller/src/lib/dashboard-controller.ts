5389ceae1b7bbdd2f6683c8d9ce4f98fe3d161bb
Jethro Fredericks <jethrofredericks.dev@protonmail.com>
2023-04-21 15:17:23 +0200

    Create function for fetching market data

--- packages/dashboard-controller/src/lib/dashboard-controller.ts
+++ packages/dashboard-controller/src/lib/dashboard-controller.ts
@@ -1,3 +1,12 @@
-export function dashboardController(): string {
-  return 'dashboard-controller';
+import { AxiosResponse } from 'axios';
+import 'dotenv/config';
+
+import { getClient } from '../utils/client';
+
+export async function getMarketData(
+  numResults?: number
+): Promise<AxiosResponse> {
+  const marketDataUrl = process.env.COINMARKET_CAP_MARKET_DATA_URL;
+  const client = getClient();
+  return client.get(`${marketDataUrl}&limit=${numResults || 5000}`);
 }
